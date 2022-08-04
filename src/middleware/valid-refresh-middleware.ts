991e1eb1325d9d815cf14a3038927cbcbc019727
100CallsToEurop <piaterik123123@mail.ru>
2022-08-04 22:52:22 +0300

    new dz

--- src/middleware/valid-refresh-middleware.ts
+++ src/middleware/valid-refresh-middleware.ts
@@ -4,7 +4,7 @@ import {jwtService} from "../applications/jwt-service";
 export const validRefreshToken = async (req: Request, res: Response, next: NextFunction) => {
    if(req.cookies?.refreshToken){
        const token = req.cookies.refreshToken
-       if(!token || !await jwtService.getUserIdByToken(token) || !await jwtService.expToken(token)){
+       if(!token  || await jwtService.expToken(token) === false || !await jwtService.getUserIdByToken(token)){
            res.send(401)
            return
        }
