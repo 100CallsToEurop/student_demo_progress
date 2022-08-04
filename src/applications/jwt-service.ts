eaf8324c3535f2db6ea0ddfdfd7051ef535fe2f0
100CallsToEurop <piaterik123123@mail.ru>
2022-08-04 23:57:36 +0300

    new dz

--- src/applications/jwt-service.ts
+++ src/applications/jwt-service.ts
@@ -27,18 +27,5 @@ export const jwtService = {
       }catch(err){
          return false
       }
-   },
-
-   createInvalidToken(token: string){
-      try {
-         const decode: any = jwt.verify(token, '123')
-         const badToken = jwt.sign({userId: decode.userId, iat: 1659645696, exp: 1659645696}, '123', {
-            expiresIn: 20,
-            notBefore: 20
-         })
-         return badToken
-      }catch(err){
-         return null
-      }
    }
 }
