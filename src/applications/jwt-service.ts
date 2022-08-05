63775d006a448f0fa86f2100548e663425feb3e5
100CallsToEurop <piaterik123123@mail.ru>
2022-08-05 12:32:49 +0300

    new d

--- src/applications/jwt-service.ts
+++ src/applications/jwt-service.ts
@@ -3,8 +3,8 @@ import {UserViewModel} from "../types/user.type";
 
 export const jwtService = {
     async createJWT(user: UserViewModel) {
-        const accessToken = jwt.sign({userId: user.id}, '123', {expiresIn: 10, notBefore: 10})
-        const refreshToken = jwt.sign({userId: user.id}, '123', {expiresIn: 20, notBefore: 20})
+        const accessToken = jwt.sign({userId: user.id}, '123', {expiresIn: 10})
+        const refreshToken = jwt.sign({userId: user.id}, '123', {expiresIn: 20})
         return {
             accessToken,
             refreshToken
